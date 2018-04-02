a2b4b756d95dbdbf20abb6d12990e9cfb973b0a6
Pippin Barr <pippin.barr@gmail.com>
2018-04-02 09:38:40 -0400

    Added shivering and pulsing tests
    Started the process of adding actual output modalities to see how they feel and to test key issues like whether you can still slide the slider if it's animating.
    
    Both pulsing and shivering look pretty effective (and work with sliding).

--- js/script.js
+++ js/script.js
@@ -7,6 +7,13 @@ Sexy?
 
 **********************************************/
 
+var shivering = false;
+var shiverAmount = 2;
+var shiverFrequency = 5;
+
+var pulsing = false;
+var pulseSize = 52;
+
 $(document).ready(function () {
 
   // Set up the jQuery UI slider
@@ -20,14 +27,80 @@ $(document).ready(function () {
 
   // Fit the slider in the window and handle resizing
   fitSliderToViewport();
-  $(window).on('resize',fitSliderToViewport)
+  $(window).on('resize',fitSliderToViewport);
+
+  $(window).on('keydown',handleKeyDown);
+
+  window.requestAnimationFrame(update);
 });
+
+
+function update() {
+  window.requestAnimationFrame(update);
+}
+
 
 // slide()
 //
 // Called when the slider is moved
 function slide(event,ui) {
   console.log(ui.value);
+}
+
+
+function handleKeyDown(event) {
+  switch (event.key) {
+    case 'a':
+    startShiver();
+    break;
+
+    case 'b':
+    shivering = false;
+    break;
+
+    case 'c':
+    startPulse();
+    break;
+
+    case 'd':
+    pulsing = false;
+    break;
+  }
+}
+
+function startShiver() {
+  if (shivering) return;
+  shivering = true;
+  shiver();
+}
+
+function shiver() {
+  $('#slider').effect('shake',{
+    distance: shiverAmount,
+    times: shiverFrequency
+  },function () {
+    if (shivering) {
+      shiver();
+    }
+  });
+}
+
+function startPulse() {
+  $('#slider').addClass('pulse');
+
+  // if (pulsing) return;
+  // pulsing = true;
+  // pulse();
+}
+
+function pulse() {
+  $('#slider').animate({
+    height: pulseSize + 'vh'
+  },function () {
+    if (pulsing) {
+      pulse();
+    }
+  });
 }
 
 // fitSliderToViewport()
