5662a84c1a195903da28d92c636dd271ecb5de01
Pippin Barr <pippin.barr@gmail.com>
2018-04-12 13:46:20 -0400

    + Added simple generation of upper and lower range + instructions + detection of successful number of strokes. + Added simple connection between sliding to correct next value and tone synthesis for feedback.
    ? Ran into the kind of obvious issue around precision. It's easy to slide beyond the upper and below the lower and still catch them 'successfully' making the whole thing kind of loose. On the one hand, that seems good in the sense that you should have some play in the system, but on the other hand it maybe makes any given motion less meaningful. It did feel kind of unnatural, though, to try to slide very specifically between a set range.
    
    ? I wondered whether it might be better to have a visual indication of range rather than the numbers (and maybe even not having the pips and numbering on the slider) as a way of making it feel a little more "naturalistic"? But do you just end up in the same situation? And in a way aren't the numbers better for maintaining this idea of interface and computation as opposed to things that might be more overtly user experience-y or even organic?
    
    ? The synthesis tone changes are quite satisfying and I can imagine that the base frequency going up with arousal will sound pretty appealing (possible even introducing harmonics now that I think about it?).

--- js/script.js
+++ js/script.js
@@ -7,16 +7,22 @@ Sexy?
 
 **********************************************/
 
-var shivering = false;
-var shiverAmount = 2;
-var shiverFrequency = 5;
-
-var pulsing = false;
-var pulseSize = 52;
+var MAX = 100;
+var MIN = 0;
 
 var breathSFX;
+var toneSFX;
+
+var synth;
+var baseFrequency = 110;
 
 var selected = 50;
+
+var upper = 70;
+var lower = 40;
+var previous = lower;
+var slidesRequired = 10;
+var slides = 0;
 
 $(document).ready(function () {
 
@@ -25,22 +31,26 @@ $(document).ready(function () {
     breathSFX.play();
   }
 
+  toneSFX = new Audio('audio/tone.wav');
+
+  setupFlocking();
+
   // Set up the jQuery UI slider
   $("#slider").slider({
-      orientation: "vertical",
-      min: 0,
-      max: 100,
-      value: selected,
-      // step: 10,
-      slide: slide,
-    }).slider('pips', {
-      step: 10,
-      rest: 'label',
-      labels: ['0','','','','','','','','','','1','','','','','','','','','','2','','','','','','','','','','3','','','','','','','','','','4','','','','','','','','','','5','','','','','','','','','','6','','','','','','','','','','7','','','','','','','','','','8','','','','','','','','','','9','','','','','','','','','','10']
-    });
+    orientation: "vertical",
+    min: 0,
+    max: 100,
+    value: selected,
+    // step: 10,
+    slide: slide,
+  }).slider('pips', {
+    step: 10,
+    rest: 'label',
+    labels: ['0','','','','','','','','','','1','','','','','','','','','','2','','','','','','','','','','3','','','','','','','','','','4','','','','','','','','','','5','','','','','','','','','','6','','','','','','','','','','7','','','','','','','','','','8','','','','','','','','','','9','','','','','','','','','','10']
+  });
 
-    // This isn't quite working
-    $('.ui-slider-pips').not('#slider').unbind('mousedown');
+  // This isn't quite working
+  $('.ui-slider-pips').not('#slider').unbind('mousedown');
 
   // Fit the slider in the window and handle resizing
   fitSliderToViewport();
@@ -52,13 +62,35 @@ $(document).ready(function () {
 
   $slider = $('#slider');
 
+  generateInstruction();
 });
+
+function setupFlocking() {
+  // To use flocking we have to initialise the (sound) environment
+  var environment = flock.init();
+
+  // ... and start it.
+  environment.start();
+
+  synth = flock.synth({
+    synthDef: {
+      id: "carrier", // A unique id
+      ugen: "flock.ugen.sin", // The kind of synth
+      freq: 110 // The frequency of the tone
+    },
+  });
+}
 
 
 function update() {
   window.requestAnimationFrame(update);
 
-  console.log(selected);
+  // Set the frequency of our synth to the new frequency
+  if (selected) {
+    // synth.set({
+    //   "carrier.freq": baseFrequency + 27.5 * selected/10,
+    // });
+  }
 }
 
 
@@ -66,12 +98,47 @@ function update() {
 //
 // Called when the slider is moved
 function slide(event,ui) {
+  findSelected(ui);
+
+  if (selected === upper && previous === lower) {
+    previous = upper;
+    slides++;
+    synth.set({
+      "carrier.freq": baseFrequency + 27.5 * selected/10,
+    });
+  }
+  else if (selected === lower && previous === upper) {
+    previous = lower;
+    slides++;
+    synth.set({
+      "carrier.freq": baseFrequency + 27.5 * selected/10,
+    });
+  }
+
+  if (slides >= slidesRequired) {
+    slides = 0;
+    generateInstruction();
+  }
+}
+
+function generateInstruction() {
+  lower = Math.floor((Math.random() * (MAX - 10))/10) * 10;
+  upper = Math.floor((lower + 10 + Math.random() * (MAX - lower))/10) * 10;
+  previous = lower;
+
+  $('#instructions').text('Slide between ' + lower/10 + ' and ' + upper/10);
+}
+
+function findSelected(ui) {
   var tens = Math.round(ui.value / 10) * 10;
   var remainder = Math.abs(ui.value - tens);
   $('.ui-slider-pip').removeClass('ui-slider-pip-selected');
   if (remainder <= 3) {
     selected = tens;
     $('.ui-slider-pip-' + selected).addClass('ui-slider-pip-selected');
+  }
+  else {
+    selected = undefined;
   }
 }
 
