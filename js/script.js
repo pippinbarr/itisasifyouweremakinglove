66f1cc74197039b2453177e3a51b88671de25e9d
Pippin Barr <pippin.barr@gmail.com>
2018-06-08 16:26:02 -0400

    Added more controlled stroke ranges and strokes required, stroke timer, rearranged code, added title splash
    # Controlled strokes
    
    Now have stroke ranges and numbers set up with arrays so that it's easier to tune to specific stroke formats and numbers, it works well I think and makes more sense in terms of being able to control the experience the player's having - there are particular kinds of strokes that are sexier, I think, than others (e.g. 0 to 10 is hotter than 1 to 7, say). I mean, this is getting pretty weird, but I think it's true. Likewise, the number of strokes required is a finesse thing - you particularly don't want too few I think, though perhaps occasionally it's good.
    
    # Stroke timer
    
    Not doing anything with it yet, but the code now counts frames between selections so you know how long a stroke took. I was impressed by how it felt like something even to do that. And actually this makes me wonder whether actually displaying the current average stroke time or something makes some sense? Will think about it. Could be part of the feedback? Like "slow down, you're going at X-strokes-per-second"? This also points me at another problem which is how likely the player is to pay attention to the speed feedback. Should that be more front and centre with the slider? I feel like I'm gradually obviating the instruction panel though? Need to think about that more...
    
    # Title splash
    
    Wanted to have one of those retro 'card style' splash screens while the application loads. Ended up using Microsoft Windows 6.0 as a reference point, which has mostly text and then a clip-arty drawing of a pen (e.g. a kind of indexical sign?). Created something roughly like that but with a picture of a human heart (pixellated). Originally used a really amazing, bloody pig's heart an artist had taken a photo of, which looked very good, but for copyright purposes went across to a royalty free image which works well too. Definitely adds a lot to the idea of this as 'real' software, so I think that's a big win. It's fun.
    
    Note that in using the image of a heart as an organ I'm sort of gesturing toward this idea that the software is machine-made in some way and that they've made a slight error in picking the wrong kind of heart. It's not clear to me how far down that road I want to think (like 'what would the AI do/design?'), but I find it amusing, and it signals something 'weird' is going on from the beginning. I wonder now whether the icon for the application should be the/a heart too?

--- js/script.js
+++ js/script.js
@@ -10,26 +10,39 @@ Sexy?
 var MAX = 10;
 var MIN = 0;
 
-var breathSFX;
-var toneSFX;
+const AROUSAL_PER_STROKE = 0.01;
+var arousal = 0;
 
-var synth;
-var baseFrequency = 110;
+var strokesRequired = [5,10,10,10,15,15,16,20,20,25];
+var currentStrokesRequired;
+
+var strokeRanges = [
+  { low: 0, high: 10 },
+  { low: 0, high: 5 },
+  { low: 0, high: 2 },
+  { low: 5, high: 10 },
+  { low: 0, high: 3 },
+  { low: 0, high: 2 },
+  { low: 2, high: 10 },
+  { low: 5, high: 7 },
+  { low: 8, high: 10 },
+  { low: 9, high: 10 },
+  { low: 3, high: 7 },
+  { low: 4, high: 8 },
+  { low: 2, high: 6 },
+];
+var currentStrokeRange;
+
+var target;
+
+
+var strokes = 0;
+var currentStrokeTime = 0;
 
 var selected = 5;
-
-var upper = 7;
-var lower = 4;
-var target = lower;
-var slidesRequired = 10;
-var slides = 0;
-
 const SELECTED_MINIMUM_FRAMES = 5;
 var currentlySelected = false;
 var selectedTimer = 0;
-
-var progress = 0;
-var lastValue = selected;
 
 $(document).ready(function () {
 
@@ -37,24 +50,194 @@ $(document).ready(function () {
   createApp();
   createInputDialog();
 
-  openApp();
+  // openApp();
+
   // showInputDialog();
 
-  // $('#app-icon').on('click', function () {
-  //   setTimeout(function () {
-  //     $('#title-screen').fadeIn(250);
-  //     setTimeout(function () {
-  //       openApp();
-  //       $('#title-screen').hide();
-  //     },5000);
-  //   },500);
-  //   startupSFX.play();
-  // });
+  $('#app-icon').on('click', function () {
+    setTimeout(function () {
+      $('#title-screen').fadeIn(100);
+      setTimeout(function () {
+        $('#title-screen').fadeOut(100,function () {
+          setTimeout(openApp,500);
+        });
+      },5000);
+    },500);
+    startupSFX.play();
+  });
 
   // Start the update loop
   window.requestAnimationFrame(update);
 
 });
+
+
+
+// update()
+//
+// Called every frame
+function update() {
+
+  window.requestAnimationFrame(update);
+
+  // Increase the stroke time
+  currentStrokeTime++;
+
+  // Check if a pip is selected
+  // (Do I need to worry about this happening behind a modal?)
+  if (currentlySelected) {
+    // Increase the time the selection has been held
+    selectedTimer++;
+    // Check if it's been held long enough
+    if (selectedTimer > SELECTED_MINIMUM_FRAMES) {
+      // Arousal goes up!
+      arousal += AROUSAL_PER_STROKE;
+      $progress.progressbar({
+        value: arousal * 100
+      });
+
+      if (arousal >= 1.0) {
+        // Handle the orgasm!
+      }
+
+      // This counts as a stroke of the slider
+      strokes++;
+
+      // Check the stroke time
+      console.log(currentStrokeTime);
+
+      // Reset the stroke time
+      currentStrokeTime = 0;
+
+      // Feedback sound
+      clickSFX.play();
+
+      // Check if this most recent slide brought us to the end of this sequence
+      if (strokes >= strokesRequired[currentStrokesRequired]) {
+        strokes = 0;
+        setNewStrokeRange();
+      }
+      else {
+        // Check if this slide was to the upper or lower end of the range
+        // and swap the target based on that
+        if (target == strokeRanges[currentStrokeRange].high) {
+          target = strokeRanges[currentStrokeRange].low;
+        }
+        else {
+          target = strokeRanges[currentStrokeRange].high;
+        }
+      }
+
+      // Highlight the new target
+      highlightTarget();
+
+      // Reset the timer and the selection tracking
+      selectedTimer = 0;
+      currentlySelected = false;
+    }
+  }
+
+}
+
+
+// slide()
+//
+// Called when the slider is moved
+function slide(event,ui) {
+
+  findSelected(ui);
+
+  if (selected == target) {
+    currentlySelected = true;
+  }
+  else {
+    currentlySelected = false;
+    selectedTimer = 0;
+  }
+}
+
+
+// generateInstruction()
+//
+// Choose a lower and upper bound for the range and tell the user
+function setNewStrokeRange() {
+
+  currentStrokeRange = Math.floor(Math.random() * strokeRanges.length);
+  currentStrokesRequired = Math.floor(Math.random() * strokesRequired.length);
+
+  if (Math.random() < 0.5) {
+    target = strokeRanges[currentStrokeRange].high;
+  }
+  else {
+    target = strokeRanges[currentStrokeRange].low;
+  }
+
+  highlightTarget();
+
+  // Feedback that a new range has been selected
+  notifySFX.play();
+}
+
+
+function highlightTarget() {
+  // Issue instruction
+  $instruction.text('Set me to ' + target + '.');
+
+  // Clear formatting of pips
+  $('.ui-slider-pip').css({
+    fontWeight: 'normal',
+    border: 'none',
+    backgroundColor: 'transparent'
+  });
+
+  // Highlight the target pip
+  $('.ui-slider-pip-' + target).css({
+    // fontWeight: 'bold',
+    border: 'solid 1px black',
+    backgroundColor: 'white'
+  });
+}
+
+
+// findSelected()
+//
+// Work out which pip is selected right now (if any)
+function findSelected(ui) {
+  selected = ui.value;
+}
+
+function openApp() {
+  $app.dialog('open');
+  setNewStrokeRange();
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+//
+// SETUP
+//
+
+
+
+
+// Loading the sound effects
+
+function loadSounds() {
+  music = new Howl({
+    src: ['audio/music.wav'],
+    loop: true,
+    volume: 0.5
+  });
+  chimesSFX = new Audio('audio/chimes.wav');
+  dingSFX = new Audio('audio/ding.wav');
+  negativeSFX = new Audio('audio/chord.wav');
+  fanfareSFX = new Audio('audio/tada.wav');
+  clickSFX = new Audio('audio/start.wav');
+  climaxAlertSFX = new Audio('audio/ir_inter.wav');
+  startupSFX = new Audio('audio/startup.wav');
+  shutdownSFX = new Audio('audio/shutdown.wav');
+  notifySFX = new Audio('audio/notify.wav');
+}
 
 
 function createApp() {
@@ -72,13 +255,13 @@ function createApp() {
     buttons: {},
     closeOnEscape: false
   });
+
   // Get rid of the 'x' button on the menu bar
   $app.parent().find(".ui-dialog-titlebar-close").hide();
 
   // Make the app stay centred on window shenanigans
   $(window).resize(function() {
-    console.log("Yay");
-      $app.dialog("option", "position", { my: "center", at: "center", of: window });
+    $app.dialog("option", "position", { my: "center", at: "center", of: window });
   });
 
   // Set up the slider
@@ -90,44 +273,41 @@ function createApp() {
     value: selected,
     slide: slide,
   });
+
+  // Set up the pips on the slider
   $slider.slider('pips',{
     step: 1,
     first: 'label',
     last: 'label',
     rest: 'label',
-    // labels: ['<–'],
     labels: ['0','1','2','3','4','5','6','7','8','9','10'],
-    // ⬅
   });
 
+  // Remember the instructions panel
   $instruction = $('#instruction');
 
+  // Remember the output panel
   $output = $('#output');
-  $output.text('That feels good.');
+  $output.text('');
 
   // Set up the progress bar
   $progress = $('#progress');
   $progress.progressbar();
   $progress.progressbar({
-    value: progress*100
+    value: arousal*100
   });
 
+  // Listen for clicks on the music radio buttons
   $('#music-on').on('click',function () {
     music.play();
   });
   $('#music-off').on('click',function () {
     music.pause();
   });
-
-}
-
-function openApp() {
-  $app.dialog('open');
-  // chimesSFX.play();
-  // music.play();
 }
 
 function showInputDialog() {
+  // Trigger a mouseup on the slider to avoid the user continuing to use it
   $slider.trigger('mouseup');
   $talk.dialog('open');
   chimesSFX.play();
@@ -177,121 +357,4 @@ function createInputDialog() {
     closeOnEscape: false
   });
   $talk.parent().find(".ui-dialog-titlebar-close").hide();
-}
-
-// update()
-//
-// Called every frame
-function update() {
-  window.requestAnimationFrame(update);
-  if (currentlySelected) {
-    selectedTimer++;
-    if (selectedTimer > SELECTED_MINIMUM_FRAMES) {
-      slides++;
-
-      clickSFX.play();
-
-      if (target == upper) {
-        target = lower;
-      }
-      else {
-        target = upper;
-      }
-
-      highlightTarget();
-      selectedTimer = 0;
-      currentlySelected = false;
-    }
-
-    if (slides >= slidesRequired) {
-      slides = 0;
-      setTarget();
-      progress += 0.1;
-      $progress.progressbar({
-        value: progress * 100
-      })
-    }
-  }
-
-}
-
-
-// slide()
-//
-// Called when the slider is moved
-function slide(event,ui) {
-
-  findSelected(ui);
-
-  if (selected == target) {
-    currentlySelected = true;
-  }
-  else {
-    currentlySelected = false;
-    selectedTimer = 0;
-  }
-}
-
-
-// generateInstruction()
-//
-// Choose a lower and upper bound for the range and tell the user
-function setTarget() {
-  lower = Math.floor((Math.random() * (MAX - 1)));
-  upper = Math.floor((lower + 1 + Math.random() * (MAX - lower)));
-  while (upper == selected) {
-    upper = Math.floor((lower + 1 + Math.random() * (MAX - lower)));
-  }
-  target = upper;
-  highlightTarget();
-
-  notifySFX.play();
-}
-
-
-function highlightTarget() {
-  // Issue instruction
-  $instruction.text('Set me to ' + target + '.');
-
-  // Clear formatting of pips
-  $('.ui-slider-pip').css({
-    fontWeight: 'normal',
-    border: 'none',
-    backgroundColor: 'transparent'
-  });
-
-  // Highlight the target pip
-  $('.ui-slider-pip-' + target).css({
-    // fontWeight: 'bold',
-    border: 'solid 1px black',
-    backgroundColor: 'white'
-  });
-}
-
-
-// findSelected()
-//
-// Work out which pip is selected right now (if any)
-function findSelected(ui) {
-  selected = ui.value;
-}
-
-
-// Loading the sound effects
-
-function loadSounds() {
-  music = new Howl({
-    src: ['audio/music.wav'],
-    loop: true,
-    volume: 0.5
-  });
-  chimesSFX = new Audio('audio/chimes.wav');
-  dingSFX = new Audio('audio/ding.wav');
-  negativeSFX = new Audio('audio/chord.wav');
-  fanfareSFX = new Audio('audio/tada.wav');
-  clickSFX = new Audio('audio/start.wav');
-  climaxAlertSFX = new Audio('audio/ir_inter.wav');
-  startupSFX = new Audio('audio/startup.wav');
-  shutdownSFX = new Audio('audio/shutdown.wav');
-  notifySFX = new Audio('audio/notify.wav');
 }
